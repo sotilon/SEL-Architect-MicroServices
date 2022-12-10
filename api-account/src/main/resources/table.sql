b2bf59c3b601cf64a126df2b7e8d446f95c32222
Jaime Enriquez <jaime.sotil@gmail.com>
2022-12-10 07:04:21 -0500

    update readme  and server config

---
+++ api-account/src/main/resources/table.sql
@@ -0,0 +1,13 @@
+-- "db_bankABC_Account".account definition
+
+-- Drop table
+
+-- DROP TABLE "db_bankABC_Account".account;
+
+CREATE TABLE "db_bankABC_Account".account (
+	id int4 NOT NULL,
+	accountnumber varchar NULL,
+	minimumbalance numeric NULL,
+	currentbalance numeric NULL,
+	interestrate numeric NULL
+);
